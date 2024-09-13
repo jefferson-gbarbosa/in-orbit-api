7bf9608334c59c3676f8a332dc36b869ac5ad7bc
jefferson-gbarbosa <jeffersonqx@gmail.com>
2024-09-13 16:53:36 -0300

    :wrench: chore

--- src/http/server.ts
+++ src/http/server.ts
@@ -43,6 +43,6 @@ app.register(createGoalCompletionRoute)
 app.register(getWeekSummaryRoute)
 app.register(getWeekPendingGoalsRoute)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
